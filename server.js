9572dd29f920663387953b2c03e1192e68ae94ee
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-25 17:58:44 -0700

    Cut and paste code to sendResponse.js

--- server.js
+++ server.js
@@ -9,11 +9,8 @@ const __dirname = import.meta.dirname
 const server = http.createServer((req, res) => {
 
 
-    serveStatic(__dirname)
+    serveStatic(res, __dirname)
 
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<html><h1>The server is working!!</h1></html>')
 })
 
 server.listen(PORT, () => console.log(`Connected on port ${PORT}`))
