22cc22be8de9b6484bee43a3633706aefbe93894
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-25 17:59:47 -0700

    feat: Create function that serves the index.html file

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,5 +1,14 @@
 import path from 'node:path'
+import fs from 'node:fs/promises'
+import { sendResponse } from './sendResponse.js'
 
-export function serveStatic(baseDir) {
+export async function serveStatic(res, baseDir) {
     const filePath = path.join(baseDir, 'public', 'index.html')
+
+    try {
+        const content = await fs.readFile(filePath)
+        sendResponse(res, 200, 'text/html', content)
+    } catch (err) {
+        console.log(err)
+    }
 }
