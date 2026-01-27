ccf80a27b4c1b61d5c15271abf39d2cec0f34288
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-26 22:59:42 -0700

    Send the price data in the response

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import http from 'node:http'
 import path from 'node:path'
 import { serveStatic } from './utils/serveStatic.js'
+import { generateNewPrice } from './utils/priceSimulator.js'
+import { sendResponse } from './utils/sendResponse.js'
 
 const PORT = 3000
 
 const __dirname = import.meta.dirname
 
 const server = http.createServer((req, res) => {
+    if (req.url === "/api/price") {
+        const price = generateNewPrice()
+        sendResponse(res, 200, 'application/json', JSON.stringify({price: price}))
+        return
+    }
     serveStatic(req, res, __dirname)
 })
 
