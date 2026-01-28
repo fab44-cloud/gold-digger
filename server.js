146f824bd497d0e2eab09fa1c4649a120bd2038e
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-27 21:27:45 -0700

    Create condition for non api endpoints

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 import http from 'node:http'
-import path from 'node:path'
 import { serveStatic } from './utils/serveStatic.js'
 import { generateNewPrice } from './utils/priceSimulator.js'
 import { sendResponse } from './utils/sendResponse.js'
+import { handlePost } from './handlers/routeHandlers.js'
 
 const PORT = 3000
 
 const __dirname = import.meta.dirname
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/api") {
+const server = http.createServer( async (req, res) => {
+    if (req.url === '/api') {
         const price = generateNewPrice()
         sendResponse(res, 200, 'application/json', JSON.stringify({price: price}))
         return
     }
-    serveStatic(req, res, __dirname)
+    else if (!req.url.startsWith('/api')) {
+        return await serveStatic(req, res, __dirname)
+    }
 })
 
 server.listen(PORT, () => console.log(`Connected on port ${PORT}`))
