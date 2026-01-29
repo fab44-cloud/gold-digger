e21b77aa8e1c0cc761be82291e29d251f0b0d382
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-29 01:03:49 -0700

    feat: Create function to handle routing of data

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,3 +1,13 @@
-export function handlePost(req, res) {
-    console.log('Post request received')
+import { parseJSONBody } from "../utils/parseJSONBody.js"
+import { sendResponse } from "../utils/sendResponse.js"
+
+export async function handlePost(req, res) {
+    const data = await parseJSONBody(req)
+
+    console.log('Received data', data)
+
+    sendResponse(res, 200, 'application/json', JSON.stringify({
+        success: true,
+        message: 'Purchase saved to the ledger'
+    }))
 }
