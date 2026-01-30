99364273bbf0fec4af7748a67fd613c879025244
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-29 21:41:04 -0700

    Create function to sanatize data

---
+++ utils/santitizeInput.js
@@ -0,0 +1,14 @@
+import sanitizeHtml from 'sanitize-html'
+
+export function sanitizeInput(data) {
+    const sanitizedData = {}
+
+    for (const [key, value] of Object.entries(data)) {
+        if (typeof value === 'string') {
+            sanitizedData[key] = sanitizeHtml(value, { allowedTags: [], allowedAttributes: {}})
+        } else {
+            sanitizedData[key] = value
+        }
+    }
+    return sanitizedData
+}
