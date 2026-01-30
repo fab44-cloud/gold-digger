8a53c580d410c2cab34173f2232c0ddafc2458df
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-29 21:40:42 -0700

    Sanitize data

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -3,13 +3,15 @@ import { sendResponse } from '../utils/sendResponse.js'
 import { randomUUID } from 'node:crypto'
 import fs from 'node:fs/promises'
 import path from 'node:path'
+import { sanitizeInput } from '../utils/santitizeInput.js'
 
 export async function handlePost(req, res, __dirname) {
     try {
         const data = await parseJSONBody(req)
+        const cleanData = sanitizeInput(data)
         const transactionID = randomUUID()
     
-        const logEntry = `ID: ${transactionID}, ${new Date().toLocaleString()}, amount paid: £${data.amount}, price per Oz: £${data.price}, gold sold: ${data.ounces} Oz\n`
+        const logEntry = `ID: ${transactionID}, ${new Date().toLocaleString()}, amount paid: £${cleanData.amount}, price per Oz: £${cleanData.price}, gold sold: ${cleanData.ounces} Oz\n`
     
         await fs.appendFile(path.join(__dirname, 'purchases.txt'), logEntry)
 
