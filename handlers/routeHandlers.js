d09be6f0bd4bfb4261456ea11ac71efaa91d4e0f
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-29 19:07:30 -0700

    Log an entry of transaction data and append it to a text file

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -1,13 +1,23 @@
-import { parseJSONBody } from "../utils/parseJSONBody.js"
-import { sendResponse } from "../utils/sendResponse.js"
+import { parseJSONBody } from '../utils/parseJSONBody.js'
+import { sendResponse } from '../utils/sendResponse.js'
+import { randomUUID } from 'node:crypto'
+import fs from 'node:fs/promises'
+import path from 'node:path'
 
-export async function handlePost(req, res) {
-    const data = await parseJSONBody(req)
+export async function handlePost(req, res, __dirname) {
+    try {
+        const data = await parseJSONBody(req)
+        const transactionID = randomUUID()
+    
+        const logEntry = `ID: ${transactionID}, ${new Date().toLocaleString()}, amount paid: £${data.amount}, price per Oz: £${data.price}, gold sold: ${data.ounces} Oz\n`
+    
+        await fs.appendFile(path.join(__dirname, 'purchases.txt'), logEntry)
 
-    console.log('Received data', data)
-
-    sendResponse(res, 200, 'application/json', JSON.stringify({
-        success: true,
-        message: 'Purchase saved to the ledger'
-    }))
+        sendResponse(res, 200, 'application/json', JSON.stringify({
+            success: true,
+            transactionID: transactionID
+        }))
+    } catch(err) {
+        sendResponse(res, 500, 'application/json', JSON.stringify({error: 'Log failed'}))
+    }
 }
