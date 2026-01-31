65c1c853ca59012aa76bf7e54f13515f537b9a93
Ford Berry <173188399+fab44-cloud@users.noreply.github.com>
2026-01-30 18:42:22 -0700

    feat: Implement SSE route and verify live data stream in network tab

--- server.js
+++ server.js
@@ -20,7 +20,27 @@ setInterval(() => {
 }, 5000)
 
 const server = http.createServer( async (req, res) => {
-    if (req.url === '/api') {
+    if (req.url === '/api/updates' && req.method ==='GET') {
+        res.statusCode = 200
+
+        res.setHeader('Content-Type', 'text/event-stream')
+        res.setHeader('Cache-Control', 'no-cache')
+        res.setHeader('Connection', 'keep-alive')
+
+        const onPriceUpdate = (price) => {
+            res.write(`data: ${JSON.stringify({ price })}\n\n`)
+        }
+
+        priceEmitter.on('priceUpdate', onPriceUpdate)
+
+        req.on('close', () => {
+            priceEmitter.off('priceUpdate', onPriceUpdate)
+            res.end()
+        })
+
+        return
+    }
+    else if (req.url === '/api') {
         const price = generateNewPrice()
         sendResponse(res, 200, 'application/json', JSON.stringify({price: price}))
         return
